0dd9873400da97dc23c2006fd8282b801a746e67
ronaldscruz <ronald.scruz16@gmail.com>
2019-02-19 19:28:33 -0300

    app de buscar repositórios pronto/faltando loading e error

--- es6/src/main.js
+++ es6/src/main.js
@@ -1,2 +1,73 @@
-// Funão delay aciona o .then após 1sconst delay = () => new Promise(resolve => setTimeout(resolve, 1000));function umPorSegundo() {  delay().then(() => {    console.log('1s');    delay().then(() => {      console.log('2s');      delay().then(() => {        console.log('3s');      });    })  });}umPorSegundo();import axios from 'axios';function getUserFromGithub(user) {  axios.get(`https://api.github.com/users/${user}`)    .then(response => {      console.log(response.data);    })    .catch(err => {      console.log('Usuário não existe');    })}getUserFromGithub('diego3g');getUserFromGithub('diego3g124123');class Github {  static getRepositories(repo) {    axios.get(`https://api.github.com/repos/${repo}`)      .then(response => {        console.log(response.data);      })      .catch(err => {        console.log('Repositório não existe');
-})  }}Github.getRepositories('rocketseat/rocketseat.com.br');Github.getRepositories('rocketseat/dslkvmskv');const buscaUsuario = usuario => {  axios.get(`https://api.github.com/users/${user}`)    .then(response => {      console.log(response.data);    })    .catch(err => {      console.log('Usuário não existe');    });}buscaUsuario('diego3g')
+import api from './api'
+
+class App{
+   constructor(){
+      this.repositories = []
+      this.frmDados = document.getElementById('repo-form')
+      this.inpRepo = document.getElementById('repository')
+      this.ulRepos = document.getElementById('repo-list')
+      this.registerHandlers()
+   }
+
+   registerHandlers(){
+      this.frmDados.onsubmit = event => this.addRepository(event)
+   }
+
+   async addRepository(event){
+      event.preventDefault()
+
+      var inpRepoName = this.inpRepo.value
+
+      if(inpRepoName.length === 0){
+         return
+      }
+
+      try{
+         const resposta = await api.get(`/repos/${inpRepoName}`)
+         let {name, description, html_url, owner:{avatar_url, }} = resposta.data
+
+         this.repositories.push({
+            name, description, html_url, avatar_url
+         })
+      }catch(e){
+         console.log('Falha ao buscar da API ('+e+').')
+      }
+      
+      this.inpRepo.value = ""
+      this.inpRepo.focus()
+      this.render()
+   }
+
+   render(){
+      this.ulRepos.innerHTML = ""
+
+      this.repositories.forEach(repo => {
+         var imgAvatar = document.createElement("img")
+         imgAvatar.setAttribute('src', repo.avatar_url)
+
+         var strongRepo = document.createElement("strong")
+         var titleRepo = document.createTextNode(repo.name)
+         strongRepo.appendChild(titleRepo)
+
+         var pDesc = document.createElement("p")
+         var desc = document.createTextNode(repo.description)
+         pDesc.appendChild(desc)
+
+         var aAcessar = document.createElement('a')
+         aAcessar.href = repo.html_url
+         aAcessar.setAttribute('target', '_blank')
+         var acessar = document.createTextNode("Acessar")
+         aAcessar.appendChild(acessar)
+
+         var liItem = document.createElement("li")
+         liItem.appendChild(imgAvatar)
+         liItem.appendChild(strongRepo)
+         liItem.appendChild(pDesc)
+         liItem.appendChild(aAcessar)
+
+         this.ulRepos.appendChild(liItem)
+      })
+   }
+}
+
+new App()
