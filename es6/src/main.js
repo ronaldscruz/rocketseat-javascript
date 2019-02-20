a35181f198df27b7c1afdad60cf07e79b03d11b7
ronaldscruz <ronald.scruz16@gmail.com>
2019-02-19 22:49:40 -0300

    projeto e curso es6 finalizados

--- es6/src/main.js
+++ es6/src/main.js
@@ -2,7 +2,14 @@ import api from './api'
 
 class App{
    constructor(){
-      this.repositories = []
+      this.repositories = [
+         // {
+         //    name: 'rocketseat-javascript',
+         //    description: 'Desafios de Javascript da Rocketseat ',
+         //    html_utl: 'http://github.com/ronaldscruz/rocketseat-javascript',
+         //    avatar_url: 'https://avatars2.githubusercontent.com/u/36777554?v=4'
+         // }
+      ]
       this.frmDados = document.getElementById('repo-form')
       this.inpRepo = document.getElementById('repository')
       this.ulRepos = document.getElementById('repo-list')
@@ -11,6 +18,18 @@ class App{
 
    registerHandlers(){
       this.frmDados.onsubmit = event => this.addRepository(event)
+      this.inpRepo.onblur = () => this.inpRepo.setAttribute('style','border: 1.5px solid white;')
+   }
+
+   setLoading(isloading = true){
+      if(isloading === true){
+         var spanLoading = document.createElement("span")
+         spanLoading.setAttribute('id','loading')
+         spanLoading.appendChild(document.createTextNode(' Carregando...'))
+         this.frmDados.appendChild(spanLoading)
+      }else{
+         document.getElementById('loading').remove()
+      }
    }
 
    async addRepository(event){
@@ -22,6 +41,8 @@ class App{
          return
       }
 
+      this.setLoading()
+
       try{
          const resposta = await api.get(`/repos/${inpRepoName}`)
          let {name, description, html_url, owner:{avatar_url, }} = resposta.data
@@ -29,10 +50,14 @@ class App{
          this.repositories.push({
             name, description, html_url, avatar_url
          })
+
+         this.inpRepo.setAttribute('style','border: 1.5px solid white;')
       }catch(e){
          console.log('Falha ao buscar da API ('+e+').')
+         this.inpRepo.setAttribute('style','border: 1.5px solid red;')
       }
       
+      this.setLoading(false)
       this.inpRepo.value = ""
       this.inpRepo.focus()
       this.render()
@@ -67,6 +92,10 @@ class App{
 
          this.ulRepos.appendChild(liItem)
       })
+
+      if(this.repositories.length < 1){
+         this.ulRepos.innerHTML = `<h4 style='margin: 25px 5px; color: white'>Nada foi encontrado. Tente novamente :/ </h4>`
+      }
    }
 }
 
